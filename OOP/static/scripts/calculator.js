2759c74e1165d0b59fc7d4bb0aa1682d44438269
AslanDzhatdoev <aslandzhatdoev503@gmail.com>
2025-04-09 11:25:06 +0300

    Update calculator.js

--- OOP/static/scripts/calculator.js
+++ OOP/static/scripts/calculator.js
@@ -3,18 +3,21 @@ function addToExpression(value) {
     expr.value += value;
 }
 
-function clearExpression() {
-    document.getElementById('expression').value = '';
-    document.getElementById('calc_result').innerText = '';
-}
-
 async function calculate() {
-    const expression = document.getElementById('expression').value;
+    const input = document.getElementById('expression');
+    const expression = input.value; // без autoMultiply
+
     const response = await fetch('/calculate', {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: `expression=${encodeURIComponent(expression)}`
     });
+
     const result = await response.text();
     document.getElementById('calc_result').innerText = result;
+}
+
+function clearExpression() {
+    document.getElementById('expression').value = '';
+    document.getElementById('calc_result').innerText = '';
 }
