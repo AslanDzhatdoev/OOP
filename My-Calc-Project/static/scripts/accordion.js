0ddf7523d7b54510137c9289b1233fc265bab32c
AslanDzhatdoev <aslandzhatdoev503@gmail.com>
2025-04-09 13:14:23 +0300

    Delete My-Calc-Project directory

--- My-Calc-Project/static/scripts/accordion.js
+++
@@ -1,9 +0,0 @@
-header.addEventListener("click", () => {
-  item.classList.toggle("open");
-
-  if (item.classList.contains("open")) {
-    body.style.maxHeight = body.scrollHeight + "px";
-  } else {
-    body.style.maxHeight = "0px";
-  }
-});
